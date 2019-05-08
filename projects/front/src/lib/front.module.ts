e393b575a6b6a97ad11f37fae1c3fccf612dc205
Mateusz Stefańczyk <mateusz.stefanczyk@valueadd.pl>
2019-05-08 15:48:08 +0200

    Install angular material (WIP)

--- projects/front/src/lib/front.module.ts
+++ projects/front/src/lib/front.module.ts
@@ -1,11 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TestFormComponent } from './components/test-form/test-form.component';
 import { FrontRoutingModule } from './front-routing.module';
 
 @NgModule({
   declarations: [TestFormComponent],
-  imports: [CommonModule, ReactiveFormsModule, FrontRoutingModule]
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FrontRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule
+  ]
 })
 export class FrontModule {}
