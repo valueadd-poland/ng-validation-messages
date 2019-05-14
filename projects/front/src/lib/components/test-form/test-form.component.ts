37dc737d98888e26a04335048715514c0dbaa04a
Mateusz Stefańczyk <mateusz.stefanczyk@valueadd.pl>
2019-05-14 10:53:21 +0200

    feat(component): VNVM-8 - Refactor usage of api error messages

--- projects/front/src/lib/components/test-form/test-form.component.ts
+++ projects/front/src/lib/components/test-form/test-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApiErrorMessage } from '../../../../../validation-messages/src/lib/resources/interfaces';
 
 const validationConfig = {
   nameMinLength: 4,
@@ -13,7 +14,12 @@ const validationConfig = {
 })
 export class TestFormComponent implements OnInit {
   form: FormGroup;
-
+  apiErrors: ApiErrorMessage[] = [
+    { property: 'dodo', message: 'DoDuCheck' },
+    { property: 'dodo', message: 'DoDuCheck' }
+  ];
+  apiError: ApiErrorMessage = { property: 'dodo', message: 'DoDuCheck' };
+  multiple = false;
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
