ff5bfe345d2e82159b01678dad2f0361b7c197ba
Mateusz Do Duc <mateusz.doduc@valueadd.pl>
2019-05-17 15:55:30 +0200

    fix(project): remove barrel for services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ValidationMessagesConfig } from '../../projects/validation-messages/src/lib/resources/interfaces';
-import { ValidationMessagesService } from '../../projects/validation-messages/src/lib/services';
+import { ValidationMessagesService } from '../../projects/validation-messages/src/lib/services/validation-messages.service';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 
